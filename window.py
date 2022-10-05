6c496a0166ed550e271b42ee0bac649dedb25477
Uxeron <vgabrielius123@gmail.com>
2022-10-05 23:53:50 +0300

    Added system tray icon

--- window.py
+++ window.py
@@ -2,6 +2,11 @@ import tkinter as tk
 import win32con, win32api, pywintypes
 import sys, os
 
+from infi.systray import SysTrayIcon
+# NOTE on systray:
+# It attempts to import pkg_resources in its __init__.py file.
+# That call must be removed for packaging into .exe to work.
+
 root = tk.Tk()
 
 from fly import Fly
@@ -31,6 +36,9 @@ hWindow = pywintypes.HANDLE(int(root.frame(), 16))
 exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
 win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
 
+systray = SysTrayIcon(os.path.join(path_prefix, "fly.ico"), "Fly", on_quit=(lambda *_: root.quit()))
+systray.start()
+
 def move_fly():
     if fly.image_index == 0:
         label.configure(image=root.image_fly_2)
@@ -47,3 +55,5 @@ root.after(33, move_fly)
 
 label.pack()
 label.mainloop()
+
+systray.shutdown()
