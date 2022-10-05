ad9f902119e712e6c74ad1555131483298c0fbed
Uxeron <vgabrielius123@gmail.com>
2022-10-05 18:31:12 +0300

    Replaced Qt5 with Tkinter to reduce file size

--- window.py
+++ window.py
@@ -1,52 +1,49 @@
+import tkinter as tk
+import win32con, win32api, pywintypes
 import sys, os
 
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+root = tk.Tk()
 
 from fly import Fly
 
 fly = Fly()
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-
-window.setAttribute(Qt.WA_TranslucentBackground, True)
-window.setAttribute(Qt.WA_TransparentForMouseEvents, True)
-window.setAttribute(Qt.WA_NoSystemBackground, True)
-window.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-
-label = QLabel(window)
-
 path_prefix = ""
 if getattr(sys, 'frozen', False):
     path_prefix = sys._MEIPASS
 
-pixmap_fly_1 = QPixmap(os.path.join(path_prefix, "fly_1.png"))
-pixmap_fly_2 = QPixmap(os.path.join(path_prefix, "fly_2.png"))
-fly.size = pixmap_fly_1.width()
+# The image must be stored to Tk or it will be garbage collected.
+root.image_fly_1 = tk.PhotoImage(file=os.path.join(path_prefix, "fly_1.png"))
+root.image_fly_2 = tk.PhotoImage(file=os.path.join(path_prefix, "fly_2.png"))
 
-label.setPixmap(pixmap_fly_1)
-label.setGeometry(0, 0, fly.size, fly.size)
+label = tk.Label(root, image=root.image_fly_1, bg='white')
 
-window.label = label
+fly.size = root.image_fly_1.width()
 
-window.resize(fly.size, fly.size)
+root.overrideredirect(True)
+root.geometry(f"{fly.size}x{fly.size}+255+255")
+root.lift()
+root.wm_attributes("-topmost", True)
+root.wm_attributes("-disabled", True)
+root.wm_attributes("-transparentcolor", "white")
+
+hWindow = pywintypes.HANDLE(int(root.frame(), 16))
+exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
 
 def move_fly():
     if fly.image_index == 0:
-        label.setPixmap(pixmap_fly_2)
+        label.configure(image=root.image_fly_2)
         fly.image_index = 1
     else:
-        label.setPixmap(pixmap_fly_1)
+        label.configure(image=root.image_fly_1)
         fly.image_index = 0
 
-    new_pos = fly.move((window.pos().x(), window.pos().y()))
-    window.move(new_pos[0], new_pos[1])
+    new_pos = fly.move((root.winfo_x(), root.winfo_y()))
+    root.geometry(f"{fly.size}x{fly.size}+{new_pos[0]}+{new_pos[1]}")
+    root.after(33, move_fly)
 
-timer=QTimer()
-timer.timeout.connect(move_fly)
-timer.start(33)
+root.after(33, move_fly)
 
-window.show()
-sys.exit(app.exec_())
+label.pack()
+label.mainloop()
